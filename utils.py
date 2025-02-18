e62697c49adc9a01426c9a70e29c408b306e9eab
tonybnya <nya.tony2010@gmail.com>
2025-02-18 16:50:39 +0100

    feat: add utils.py

---
+++ utils.py
@@ -0,0 +1,187 @@
+katas = [
+    "52685f7382004e774f0001f7",
+    "514b92a657cdc65150000006",
+    "54ba84be607a92aa900000f1",
+    "526571aae218b8ee490006f4",
+    "517abf86da9663f1d2000003",
+    "50654ddff44f800200000004",
+    "5acd142a2ec8c48521000104",
+    "67b4640ecc0a6ce3f6457562",
+    "550f22f4d758534c1100025a",
+    "61711668cfcc35003253180d",
+    "5a036ecb2b651d696f00007c",
+    "67b3aa6052e115b1dd4575df",
+    "5710443187a36a9cee0005a1",
+    "5576f6719988e71ea30000ae",
+    "67857e40220da0b5a0e7cae2",
+    "52d9b893b2fcb0b76c000029",
+    "65e3a41ea4fa620065a14aaf",
+    "669e2bf9d110407eb4dc3c56",
+    "57eb8fcdf670e99d9b000272",
+    "5868b2de442e3fb2bb000119",
+    "67b2a5b03ce5bda0cb889384",
+    "53fc954904a45eda6b00097f",
+    "559323e59ddea3c94800006f",
+    "52d43d5515be7cbc92000611",
+    "52cb0b50c61b330f18000763",
+    "526bd612696e23c43a00032e",
+    "57c4ed873189a5bea00007e6",
+    "5d325ae1dddb56dc5b740738",
+    "58a3ea0a8bdda5093800000e",
+    "5889a0706069af1cb9000176",
+    "58882bdb5edef0343400002a",
+    "582957651c1f59b99b0000aa",
+    "55f07ddd97090cf03300006b",
+    "528da9e9be46d138960006be",
+    "59d9d8cb27ee005972000045",
+    "5600e00e42bcb7b9dc00014e",
+    "67a61fa0ed1d3932f2c39b66",
+    "5592e3bd57b64d00f3000047",
+    "565c0fa6e3a7d39dee000125",
+    "5544c7a5cb454edb3c000047",
+    "55e7280b40e1c4a06d0000aa",
+    "56445c4755d0e45b8c00010a",
+    "566be96bb3174e155300001b",
+    "67a88017e4f6c78e971870ee",
+    "59bf943cafcda28e31000130",
+    "550498447451fbbd7600041c",
+    "5700c9acc1555755be00027e",
+    "563f0c54a22b9345bf000053",
+    "55e6f5e58f7817808e00002e",
+    "564057bc348c7200bd0000ff",
+    "5a057ec846d843c81a0000ad",
+    "5ee98cf315741d00104499e5",
+    "57fb09ef2b5314a8a90001ed",
+    "5705c699cb729350870003b7",
+    "5cc1e284ece231001ccf7014",
+    "57a31ce7cf1fa5a1e1000227",
+    "6790dcd817b22cc890f673bd",
+    "5995ff073acba5fa3a00011d",
+    "5ebd53ea50d0680031190b96",
+    "5a516c2efd56cbd7a8000058",
+    "56dd927e4c9055f8470013a5",
+    "56ee0448588cbb60740013b9",
+    "56d9b46113f38864b8000c5a",
+    "53697be005f803751e0015aa",
+    "5a30e7e9c5e28454790000c1",
+    "5fc7caa854783c002196f2cb",
+    "58e18c5434a3022d270000f2",
+    "5963c18ecb97be020b0000a2",
+    "5b085335abe956c1ef000266",
+    "5eb9a92898f59000184c8e34",
+    "57e35f1bc763b8ccce000038",
+    "5a4d303f880385399b000001",
+    "67a758673e6850c1bf72385b",
+    "64cf783d39f59a002a8f128f",
+    "5cd12646cf44af0020c727dd",
+    "5946a0a64a2c5b596500019a",
+    "5a1cb5406975987dd9000028",
+    "5a0aae48ba2a14cfa600016d",
+    "5e28b3ff0acfbb001f348ccc",
+    "59923f1301726f5430000059",
+    "55f4ad47ada1dd22f1000088",
+    "58a5aeb893b79949eb0000f1",
+    "5276c18121e20900c0000235",
+    "565f5825379664a26b00007c",
+    "5f579a3b34d5ad002819eb9e",
+    "58e230e5e24dde0996000070",
+    "5b234007d74b5b90f20000a8",
+    "5b5e0c0d83d64866bc00001d",
+    "55832eda1430b01275000059",
+    "5870fa11aa0428da750000da",
+    "5dad6e5264e25a001918a1fc",
+    "5a622f5f85bef7a9e90009e2",
+    "59dd3ccdded72fc78b000b25",
+    "5b39e91ee7a2c103300018b3",
+    "56b0f5f84de0afafce00004e",
+    "5e5b7f55c2e8ae0016f42339",
+    "56fcc1ee45040039ab0016da",
+    "675defec0554b17b8de2b0a4",
+    "5bec507e1ab6db71110001fc",
+    "52705ed65de62b733f000064"
+]
+
+integration = {
+  "data": {
+    "date": {
+      "created_at": "2025-02-18",
+      "updated_at": "2025-02-18"
+    },
+    "descriptions": {
+      "app_description": "Posts a coding challenge every morning to sharpen developer skills.",
+      "app_logo": "https://i.postimg.cc/5Nn52jM9/codex.png",
+      "app_name": "Codex",
+      "app_url": "URL to the application or service.",
+      "background_color": "#151515"
+    },
+    "integration_category": "Development & Code Management",
+    "integration_type": "interval",
+    # "is_active": False,
+    # "output": [
+    #   {
+    #     "label": "output_channel_1",
+    #     "value": True
+    #   },
+    #   {
+    #     "label": "output_channel_2",
+    #     "value": False
+    #   }
+    # ],
+    # "key_features": [
+    #   "Feature description 1.",
+    #   "Feature description 2.",
+    #   "Feature description 3.",
+    #   "Feature description 4."
+    # ],
+    # "permissions": {
+    #   "monitoring_user": {
+    #     "always_online": True,
+    #     "display_name": "Performance Monitor"
+    #   }
+    # },
+    "settings": [
+      {
+        "label": "interval",
+        "type": "text",
+        "required": True,
+        "default": "* * * * *"
+      }
+      # {
+      #   "label": "Key",
+      #   "type": "text",
+      #   "required": True,
+      #   "default": "1234567890"
+      # },
+      # {
+      #   "label": "Do you want to continue",
+      #   "type": "checkbox",
+      #   "required": True,
+      #   "default": "Yes"
+      # },
+      # {
+      #   "label": "Provide Speed",
+      #   "type": "number",
+      #   "required": True,
+      #   "default": "1000"
+      # },
+      # {
+      #   "label": "Sensitivity Level",
+      #   "type": "dropdown",
+      #   "required": True,
+      #   "default": "Low",
+      #   "options": ["High", "Low"]
+      # },
+      # {
+      #   "label": "Alert Admin",
+      #   "type": "multi-checkbox",
+      #   "required": True,
+      #   "default": "Super-Admin",
+      #   "options": ["Super-Admin", "Admin", "Manager", "Developer"]
+      # }
+    ],
+    "tick_url": "URL for subscribing to Telex's clock.",
+    "target_url": "Optional URL for getting data from the Telex channel"
+  }
+}
+
+print(len(katas))
