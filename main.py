dac02e1d8ab0371c15417227002e3d3df9f4b104
tonybnya <nya.tony2010@gmail.com>
2025-02-19 18:13:10 +0100

    feat: add coding_challenge route to daily fetch the URL of a random challenge

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import os
+"""
+Main application.
+"""
+import asyncio
 import logging
-from flask import Flask, jsonify
+import os
+import random
+from typing import List
+import httpx
 from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from utils import integration, katas
+from pydantic import BaseModel
+from utils import katas
 
 
 # load environment variables from .env
@@ -15,6 +23,22 @@ logger = logging.getLogger(__name__)
 
 # initialize Flask app
 app = Flask(__name__)
+
+
+class Setting(BaseModel):
+    """ Setting model. """
+    label: str
+    type: str
+    required: bool
+    default: str
+
+
+class MonitorPayload(BaseModel):
+    """ Payload model. """
+    channel_id: str
+    return_url: str
+    settings: List[Setting]
+
 
 # list of allowed origins
 allowed_origins = [
@@ -30,7 +54,7 @@ CORS(app, origins=allowed_origins)
 
 @app.route("/", methods=["GET"])
 def root():
-    """ Root endpoint returning a JSON message """
+    """ Root endpoint returning a JSON message. """
     logger.info("Root endpoint accessed")
     return jsonify(
         {
@@ -40,6 +64,76 @@ def root():
             "category": "Development & Code Management"
         }
     )
+
+
+@app.route("/coding_challenge", methods=["GET"])
+def get_coding_challenge():
+    """ Endpoint to get a coding challenge. """
+    base_url = request.base_url.rstrip("/coding_challenge")
+    codewars_base_url = "https://www.codewars.com/kata"
+    kata_id = random.choice(katas)
+
+    kata = f"{codewars_base_url}/{kata_id}"
+    return jsonify(
+        {
+            "data": {
+                "date": {
+                    "created_at": "2025-02-18",
+                    "updated_at": "2025-02-18"
+                },
+                "descriptions": {
+                    "app_description": "Posts a coding challenge every morning to sharpen developer skills.",
+                    "app_logo": "https://i.postimg.cc/5Nn52jM9/codex.png",
+                    "app_name": "Codex",
+                    "app_url": base_url,
+                    "background_color": "#151515"
+                },
+                "integration_category": "Development & Code Management",
+                "integration_type": "interval",
+                "is_active": True,
+                "author": "Tony B. NYA",
+                "settings": [
+                    {
+                        "label": "interval",
+                        "type": "text",
+                        "required": True,
+                        "default": "* * * * *"
+                    }
+                ],
+                "tick_url": f"{kata}/tick",
+            }
+        }
+    )
+
+
+# def coding_challenge(payload: MonitorPayload):
+async def coding_challenge(payload: MonitorPayload):
+    """Monitor websites and send a report to the return URL."""
+    codewars_base_url = "https://www.codewars.com/kata"
+    kata_id = random.choice(katas)
+
+    kata = f"{codewars_base_url}/{kata_id}"
+
+    data = {
+        "message": kata,
+        "username": "Coding Challenge",
+        "event_name": "Uptime Check",
+        "status": "error"
+    }
+
+    async with httpx.AsyncClient() as client:
+        await client.post(payload.return_url, json=data)
+
+
+@app.route("/tick", methods=["POST"])
+def tick():
+    """Flask route to handle monitoring requests."""
+    try:
+        payload = MonitorPayload(**request.json)
+        asyncio.create_task(coding_challenge(payload))
+        return jsonify({"message": "Coding Challenge delivered"}), 202
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 if __name__ == "__main__":
